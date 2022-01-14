d1f20c18d536f42e28cc60b5a7982e5afd651f4b
unknown <coruc4206@gmail.com>
2022-01-15 00:28:14 +0300

    First POM Facebook

---
+++ src/test/java/pages/FacebookLoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FacebookLoginPage {
+    WebDriver driver;
+
+    public FacebookLoginPage(WebDriver driver) {
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy (css="#email")
+    public WebElement email;
+
+    @FindBy(css="#pass")
+    public WebElement password;
+
+    @FindBy (xpath = "//button[@value='1']")
+    public  WebElement loginButton;
+}
---
+++ src/test/java/tests/FacebookLoginTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pages.FacebookLoginPage;
+import utulities.TestBase;
+
+public class FacebookLoginTest extends TestBase {
+    @Test
+        public void facebookTest() {
+            Faker faker=new Faker();
+            driver.get("https://www.facebook.com/");
+            FacebookLoginPage facebookLoginPage=new FacebookLoginPage(driver);
+            facebookLoginPage.email.sendKeys(faker.internet().emailAddress());
+            facebookLoginPage.password.sendKeys(faker.internet().password());
+            facebookLoginPage.loginButton.click();
+
+    }
+
+}
